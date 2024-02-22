74fdc79cdba8131f190814d1be991c28e7b25295
Felipe <f.alfonso@res-ear.ch>
2024-02-21 22:10:12 -0300

    Updates - new futures

--- src/upd8all_updater.py
+++ src/upd8all_updater.py
@@ -5,6 +5,7 @@ import getpass
 import subprocess
 import select
 import pty
+import json
 
 # Function to print the welcome message
 def print_welcome_message():
@@ -26,25 +27,21 @@ def execute_command_with_sudo(command, sudo_password):
             command,
             shell=True,
             stdin=subprocess.PIPE,
-            stdout=slave,
-            stderr=slave,
-            close_fds=True
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True
         )
 
         # Send sudo password
-        os.write(master, (sudo_password + "\n").encode())
-        os.close(master)
+        sudo_prompt = proc.communicate(f"{sudo_password}\n")[1]
+        if "Sorry" in sudo_prompt:  # If "Sorry" in sudo prompt, password was incorrect
+            print("Incorrect sudo password. Exiting.")
+            sys.exit(1)
 
         # Read output
-        while True:
-            r, _, _ = select.select([slave], [], [])
-            if r:
-                output = os.read(slave, 1024).decode().strip()
-                if not output:
-                    break
-                print(output)
-
-        proc.wait()
+        stdout, stderr = proc.communicate()
+        print(stdout)
+        print(stderr)
     else:
         os.system(command)
 
@@ -52,30 +49,30 @@ def execute_command_with_sudo(command, sudo_password):
 def update_pacman(sudo_password):
     print("Updating Pacman packages...")
     print("-------------------------------------")
-    command = f"echo {sudo_password} | sudo -S pacman -Syu --noconfirm"
+    command = "sudo pacman -Syu --noconfirm"
     execute_command_with_sudo(command, sudo_password)
 
 # Function to update AUR packages with Yay
 def update_yay(sudo_password):
     print("Updating AUR packages with Yay...")
     print("-------------------------------------")
-    # Add configuration to avoid yay warning
     config_path = os.path.expanduser("~/.config/yay/")
     os.makedirs(config_path, exist_ok=True)
     config_file = os.path.join(config_path, "config.json")
     with open(config_file, "w") as f:
-        f.write('{"misc": {"save": true}}')
-    # Update yay
+        json.dump({"misc": {"save": True}}, f)
     command = "yay -Syu --noconfirm"
     execute_command_with_sudo(command, sudo_password)
 
 # Function to update packages with Homebrew
 def update_brew():
-    print("Updating packages with Homebrew...")
+    print("\nUpdating packages with Homebrew...")
     print("-------------------------------------")
     command = "brew update && brew upgrade"
-    os.system(command)
 
+    os.system(command)
+    print("\n-----------------------------------\n")
+    
 # Function to check the version of a package in a specific package manager
 def check_package_version(package, package_manager):
     if package_manager == "pacman":
@@ -94,7 +91,9 @@ def check_package_version(package, package_manager):
 # Function executed in a separate thread to show a warning message if no package name is entered within 1 minute
 def timeout_warning():
     print("\nTime's up. Program execution has ended.\n")
+    sys.stdout.flush()  # Limpiar el buffer de salida
     sys.exit(0)
+
 
 def main():
     # Print welcome message
@@ -159,7 +158,7 @@ def main():
     elif selected_option == '3' and has_brew:
         package_manager = "brew"
     else:
-        print("\nInvalid option. Exiting the program.\n")
+        print("\nInvalid option (Or, you didn't choose any option above). Exiting the program.\n")
         sys.exit(1)
 
     # Cancel timer if the user provides a package name
